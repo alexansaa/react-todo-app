41923d1f61bdf39a6ae6cea962b00332cfeb339a
alexansaa <alexan.saa@gmail.com>
2023-08-23 12:05:58 -0500

    nav bar example impl

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -49,6 +49,16 @@ const TodosLogic = () => {
     };
     setTodos([...todos, newTodo]);
   };
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
   return (
     <div>
       <InputTodo addTodoItem={addTodoItem}/>
@@ -56,6 +66,7 @@ const TodosLogic = () => {
         todosProps={todos}
         handleChange={handleChange}
         delTodo={delTodo}
+        setUpdate={setUpdate}
       />
     </div>
   );
